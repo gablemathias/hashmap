b5606cc6dcfa500be31e182e9ff142b4e2cfe10a
Gabriel Mathias <gablemathias@icloud.com>
2024-08-02 13:16:05 -0300

    add set method, helper methods for it and modify constructor

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,7 +1,13 @@
+require_relative 'linked_list'
+
 class HashMap
+  attr_reader :capacity
 
   def initialize
     @buckets = [] # Initial "size" has to be 16
+    @capacity = 16
+    @load_factor = 0.8
+    @quantity = 0
   end
 
   def hash(key)
@@ -14,6 +20,30 @@ class HashMap
   end
 
   def set(key, value)
-    
+    index = hash(key) % capacity
+
+    check_index(index)
+
+    if @buckets[index].nil?
+      @buckets[index] = LinkedList.new.append([key, value])
+    else
+      @buckets[index].append([key, value])
+    end
+
+    calibrate_capacity if buckets_overload?
   end
+  
+    private
+
+    def check_index(idx)
+      raise IndexError if index.negative? || index >= @buckets.length
+    end
+
+    def buckets_overload?
+      quantity >= capacity * @load_factor
+    end
+
+    def calibrate_capacity
+      @capacity = capacity * 2
+    end
 end
